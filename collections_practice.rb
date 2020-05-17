b1dfd5db9ef30656b6f8b5e06793eb3ae0b7c159
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-17 22:03:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,6 +45,16 @@ def sum_array(array)
   end
   sum
 end
+
+def add_s(array)
+  array.collect do |word|
+    if array[1] == word
+      word
+    else
+      word + "s"
+    end
+  end
+end
   
   
   
